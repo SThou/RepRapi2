d5b2894039a651b93f64012dd4f91013067e70b5
ST <ST@STs-MacBook-Pro-4.local>
2014-09-27 11:27:22 -0700

    Commit to check out

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -166,7 +166,6 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 
 // default settings 
 
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   {53.2,53.2,3200/1.25,1000}  // default steps per unit for ultimaker 
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {53.2,53.2,3200/1.25,625}  // default steps per unit for NextDay 
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 45}    // (mm/sec)    
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
